a740936d1cbd814d6ef07cbbda01197950268941
tianyage <454701103@qq.com>
2024-10-21 02:21:17 +0800

    fix
    解决单例并发情况下select(db)导致的冲突问题

--- src/Cache.php
+++ src/Cache.php
@@ -2,7 +2,6 @@
 
 namespace Tianyage\SimpleCache;
 
-use Closure;
 use Exception;
 use Redis;
 use RedisException;
@@ -11,8 +10,28 @@ use Throwable;
 class Cache
 {
     
-    private static redis $redis;
-    private static int   $db_index = 0;
+    private static array $instances = [];
+    
+    //    public static function getInstance(int $index): Redis
+    //    {
+    //        if (empty(self::$redis)) {
+    //            $config = self::getConfig();
+    //            try {
+    //                self::$redis = new Redis();
+    //                self::$redis->connect($config['hostname'], $config['port']);
+    //                self::$redis->auth($config['password']); //设置密码
+    //            } catch (Exception $e) {
+    //                echo '链接redis失败:' . $e->getMessage();
+    //                throw $e;
+    //            }
+    //        }
+    //        // 切换db时做判断是否一致
+    //        if ($index !== self::$db_index) {
+    //            self::$redis->select($index);
+    //            self::$db_index = $index;
+    //        }
+    //        return self::$redis;
+    //    }
     
     /**
      * redis单例
@@ -24,77 +43,61 @@ class Cache
      */
     public static function getInstance(int $index): Redis
     {
-        if (empty(self::$redis)) {
-            $config = self::getConfig();
-            try {
-                self::$redis = new Redis();
-                self::$redis->connect($config['hostname'], $config['port']);
-                self::$redis->auth($config['password']); //设置密码
-            } catch (Exception $e) {
-                echo '链接redis失败:' . $e->getMessage();
-                throw $e;
-            }
+        // 检查当前实例是否存在
+        if (!isset(self::$instances[$index])) {
+            self::createRedisInstance($index);
         }
-        // 切换db时做判断是否一致
-        if ($index !== self::$db_index) {
-            self::$redis->select($index);
-            self::$db_index = $index;
+        
+        // 检查Redis连接是否仍然有效
+        if (!self::$instances[$index]->ping()) {
+            self::reconnectRedis($index);
         }
-        return self::$redis;
+        
+        return self::$instances[$index];
     }
     
     /**
-     * 模糊查找redis key
+     * 创建新的 Redis 实例
      *
-     * @param string $pattern // 要匹配的规则  'test_*'
-     * @param int    $count   // 每次遍历数量 count越大总耗时越短，但单次阻塞越长。 建议5000-10000。并发不高则可以调至接近1w。
+     * @param int $index
      *
-     * @return array
-     * @throws
-     */
-    public static function redisScan(string $pattern, int $count = 5000): array
-    {
-        $keyArr = [];
-        while (true) {
-            // $iterator 下条数据的坐标
-            $data   = self::$redis->scan($iterator, $pattern, $count);
-            $keyArr = array_merge($keyArr, $data ?: []);
-            
-            if ($iterator === 0) {   //迭代结束，未找到匹配
-                break;
-            }
-            if ($iterator === null) {//"游标为null了，重置为0，继续扫描"
-                $iterator = "0";
-            }
-        }
-        //        $keyArr = array_flip($keyArr);
-        //        $keyArr = array_flip($keyArr);
-        return $keyArr;
-    }
-    
-    /**
-     * 批量删除（pip管道模式）
-     *
-     * @param string   $key 支持模糊搜索[*] exp: test1_*
-     * @param int|null $db  [可选] 切换db
-     *
-     * @return true
+     * @return void
      * @throws RedisException
      */
-    public static function delMutil(string $key, int $db = null): bool
+    private static function createRedisInstance(int $index): void
     {
-        if (!is_null($db)) {
-            self::$redis->select($db);
+        $config = self::getConfig();
+        try {
+            $redis = new Redis();
+            $redis->connect($config['hostname'], $config['port']);
+            $redis->auth($config['password']);
+            $redis->select($index);  // 选择对应的数据库
+            self::$instances[$index] = $redis;
+        } catch (Exception $e) {
+            echo '连接redis失败: ' . $e->getMessage();
+            throw $e;
         }
-        $keys = self::redisScan($key); // 扫描查找
-        if ($keys) {
-            $pipe = self::$redis->multi(self::$redis::PIPELINE);  // 开启管道模式，代表将操作命令暂时放在管道里
-            foreach ($keys as $key) {
-                $pipe->del($key);
-            }
-            $pipe->exec(); // 开始执行管道里所有命令
+    }
+    
+    /**
+     * 重连 Redis 实例
+     *
+     * @param int $index
+     *
+     * @return void
+     * @throws RedisException
+     */
+    private static function reconnectRedis(int $index): void
+    {
+        try {
+            //            echo "检测到 Redis 连接断开，尝试重连...\n";
+            self::$instances[$index]->close(); // 先关闭原来的连接
+            self::createRedisInstance($index); // 重新创建实例
+            //            echo "Redis 重连成功\n";
+        } catch (Exception $e) {
+            echo 'Redis 重连失败: ' . $e->getMessage();
+            throw $e;
         }
-        return true;
     }
     
     /**
@@ -142,5 +145,61 @@ class Cache
         }
         
         return $config;
+    }
+    
+    /**
+     * 模糊查找redis key
+     *
+     * @param string $pattern 要匹配的规则  'test_*'
+     * @param int    $db      数据库号 默认0
+     * @param int    $count   每次遍历数量 count越大总耗时越短，但单次阻塞越长。 建议5000-10000。并发不高则可以调至接近1w。
+     *
+     * @return array
+     * @throws RedisException
+     */
+    public static function redisScan(string $pattern, int $db, int $count = 5000): array
+    {
+        $redis = self::getInstance($db);
+        
+        $keyArr = [];
+        while (true) {
+            // $iterator 下条数据的坐标
+            $data   = $redis->scan($iterator, $pattern, $count);
+            $keyArr = array_merge($keyArr, $data ?: []);
+            
+            if ($iterator === 0) {   //迭代结束，未找到匹配
+                break;
+            }
+            if ($iterator === null) { //"游标为null了，重置为0，继续扫描"
+                $iterator = "0";
+            }
+        }
+        //        $keyArr = array_flip($keyArr);
+        //        $keyArr = array_flip($keyArr);
+        return $keyArr;
+    }
+    
+    /**
+     * 批量删除（pip管道模式）
+     *
+     * @param string $key 支持模糊搜索[*] exp: test1_*
+     * @param int    $db  数据库编号
+     *
+     * @return true
+     * @throws RedisException
+     */
+    public static function delMutil(string $key, int $db): bool
+    {
+        $redis = self::getInstance($db);
+        
+        $keys = self::redisScan($key, $db); // 模糊扫描查找
+        if (count($keys) > 0) {
+            $pipe = $redis->multi($redis::PIPELINE);  // 开启管道模式，代表将操作命令暂时放在管道里
+            foreach ($keys as $key) {
+                $pipe->del($key);
+            }
+            $pipe->exec(); // 开始执行管道里所有命令
+        }
+        return true;
     }
 }
--- tests/test.php
+++ tests/test.php
@@ -4,17 +4,19 @@ use Tianyage\SimpleCache\Cache;
 
 require_once '../vendor/autoload.php';
 
+$testdb = 9;
+
 try {
-    $cache = Cache::getInstance(8);
+    $redis = Cache::getInstance($testdb);
 } catch (Exception $e) {
     echo $e->getMessage();
     die;
 }
 
-$cache->setex('test', 20, date("Y-m-d H:i:s"));
-echo $cache->get('test') . PHP_EOL;
+$redis->setex('testfff', 20, date("Y-m-d H:i:s"));
+echo $redis->get('testfff') . PHP_EOL;
 
-$arr = Cache::redisScan('test*');
+$arr = Cache::redisScan('test*', $testdb);
 print_r($arr);
 
-var_dump(Cache::delMutil('test*'));
+var_dump(Cache::delMutil('test*', $testdb));
